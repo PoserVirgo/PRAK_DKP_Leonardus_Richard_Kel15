72d10802bf93737526817a639ca6e054e0c3f9d9
LeonardusRichard <leonardusrichard974@gmail.com>
2022-03-27 16:34:28 +0700

    Tugas

---
+++ membeli_eskrim.py
@@ -0,0 +1,76 @@
+
+def non_return_func(pelanggan1):
+    print(f"SELAMAT DATANG DI ES KRIM SERBA RASA {pelanggan1} ")
+def return_func(part):
+    print(f"Antrian ke",part)
+function = lambda pelanggan1 : print(f"Silahkan pilih menu {pelanggan1}")
+non_return_func("pelanggan 1")
+return_func(1)
+function("pelanggan 1")
+pilihan1="ya"
+pilihan="tidak"
+while pilihan1 == "ya": 
+    print("""
+    ~~~~~~~~~~~~~~~~~~~~~~
+    ES KRIM SERBA RASA
+    ~~~~~~~~~~~~~~~~~~~~~~
+    1. ES krim coklat 
+    2. Es krim vanila 
+    3. Es krim oreo
+    4. Es krim blueberry  
+    5. Es krim stawberry 
+    ~~~~~~~~~~~~~~~~~~~~~~
+    """)
+    
+    eskrim=str(input("Pilih opsi ="))
+    jumlahpesan=int(input("Beli berapa ="))
+    if eskrim == "1":
+        namaeskrim= "Es krim coklat"
+        barang=(jumlahpesan)
+        if jumlahpesan >= 2:
+            totaleskrim=int(barang)
+        else:
+            totaleskrim=int(barang)
+    elif eskrim == "2":
+        namaeskrim= "Es krim vanila"
+        barang = (jumlahpesan)
+        if jumlahpesan >= 2:
+            totaleskrim =int(barang)
+        else:
+            totaleskrim =int(barang)
+    elif eskrim == "3":
+        namaeskrim= "Es krim oreo"
+        barang=int(jumlahpesan)
+        totaleskrim=int(barang)
+    elif eskrim == "4":
+        namaeskrim= "Es krim blueberry"
+        barang=int(jumlahpesan)
+        totaleskrim = int(barang)
+    elif eskrim == "5":
+        namaeskrim= "Es krim stawberry"
+        barang=int(4500*jumlahpesan)
+        totalheskrim= int(barang)    
+    else:
+        barang = "-"
+        namaeskrim= "-"
+        pilihan1=input("menu tidak ada, silahkan coba kembali ya / tidak =")
+    
+    
+    print("====================")
+    print("ES KRIM SERBA RASA")
+    print("====================")
+    print("-------------------------")
+    print("-------------------------")
+    print("Menu Es krim :",namaeskrim)
+    print("Jumlah yand di pesan:", jumlahpesan)
+    print("--------------------------")
+    print("--------------------------")
+    pilihan1=input("apakah anda ingin membeli lagi ya / tidak =")
+else: 
+    pilihan == "tidak"
+    print("")
+    print("")
+    print("Terima kasih pelanggan ")
+    print("")
+    print("")
+    
