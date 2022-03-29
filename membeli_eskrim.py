c2ed53232a8a844012491a0895a19fdfaf7d1457
LeonardusRichard <102172158+LeonardusRichard@users.noreply.github.com>
2022-03-29 13:58:42 +0700

    Update membeli_eskrim.py

--- membeli_eskrim.py
+++ membeli_eskrim.py
@@ -4,8 +4,8 @@ def non_return_func(pelanggan1):
 def return_func(faisal): 
     print(f"Antrian ke",self)
 function = lambda pelanggan1 : print(f"Silahkan pilih menu {pelanggan1}")
-non_return_func("pelanggan 1")
-return_func(farrel)
+non_return_func("pelanggan1")
+return_func(1)
 function("pelanggan 1")
 pilihan1="ya"
 pilihan="tidak"
@@ -26,9 +26,9 @@ while pilihan1 == "ya":
     jumlahpesan=int(input("Beli berapa ="))
     if eskrim == "1":
         namaeskrim= "Es krim coklat"
-        barang=(jumlahpesan)
+        barang=(farrel)
         if jumlahpesan >= 2:
-            totaleskrim=int(barang)
+            totaleskrim=int(yuda)
         else:
             totaleskrim=int(barang)
     elif eskrim == "2":
@@ -49,7 +49,7 @@ while pilihan1 == "ya":
     elif eskrim == "5":
         namaeskrim= "Es krim stawberry"
         barang=int(4500*jumlahpesan)
-        totalheskrim= int(yuda)    
+        totalheskrim= int(barang)    
     else:
         barang = "-"
         namaeskrim= "-"
